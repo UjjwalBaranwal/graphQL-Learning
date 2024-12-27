f555158741050c06a7936b1c70be6b1fee1d744d
ujjwal <ujjwalbaranwal221715@gmail.com>
2024-12-27 12:56:45 +0530

    basic resolver function are made

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
+
+//db
+import db from "./_db.js";
 //type
-import { typeDefs } from "./schema";
+import { typeDefs } from "./schema.js";
+
+//resolver function
+const resolvers = {
+  Query: {
+    games() {
+      return db.games;
+    },
+    reviews() {
+      return db.reviews;
+    },
+    authors() {
+      return db.authors;
+    },
+  },
+};
 //server setup
 const server = new ApolloServer({
   //typeDefs -- definitions of types of data
   typeDefs,
   //resolvers
+  resolvers,
 });
 
 const { url } = await startStandaloneServer(server, {
